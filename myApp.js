ef3cf27fb2a6927d4ad0cb6252e0e27e42878606
J <j.suomaki@gmail.com>
2023-10-31 17:20:22 +0200

    css

--- myApp.js
+++ myApp.js
@@ -2,6 +2,8 @@ let express = require('express');
 let app = express();
 console.log("Hello World");
 
+app.use("/public", express.static(__dirname + "/public"))
+
 app.get("/", (req, res) => {
     //res.send("Hello Express");
     res.sendFile(__dirname + '/views/index.html');
