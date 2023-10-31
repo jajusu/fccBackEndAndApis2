0082d247459fad428ece28fc106ea791ef5f9f87
J <j.suomaki@gmail.com>
2023-10-31 20:44:25 +0200

    date

--- myApp.js
+++ myApp.js
@@ -26,6 +26,15 @@ app.get("/json", (req, res) => {
     }
 });
 
+app.get("/now", (req, res, next) => {
+    req.time = new Date().toString();
+    next();
+  },
+    (req, res) => {
+      res.send(req.time);
+    }
+  );
+  
 
 
 
